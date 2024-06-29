f3f5874a80636fd7b38da0c4523ae3c0f793c040
Mario González <mario@mariogl.com>
2024-06-29 08:12:04 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Formulario de feedback
+
+## Dependencias
+
+Recuerda instalar las dependencias con `npm install` antes de comenzar 😉.
+
+## Desarrollo
+
+Para ejecutar el proyecto en modo desarrollo, utiliza el comando `npm run dev`.
---
+++ main.js
@@ -0,0 +1,32 @@
+import "./styles.css";
+
+const form = document.querySelector(".form");
+const submitButton = document.querySelector(".button");
+
+const requiredFields = form.querySelectorAll("[required]");
+
+function showError(element) {
+  element.nextElementSibling.classList.add("visible");
+}
+
+function hideError(element) {
+  element.nextElementSibling.classList.remove("visible");
+}
+
+for (const requiredField of requiredFields) {
+  requiredField.addEventListener("keyup", () => {
+    if (requiredField.value === "") {
+      showError(requiredField);
+    }
+  });
+}
+
+submitButton.addEventListener("click", () => {
+  for (const requiredField of requiredFields) {
+    if (requiredField.value === "") {
+      showError(requiredField);
+    } else {
+      hideError(requiredField);
+    }
+  }
+});
